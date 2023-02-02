5c64a21a36694bce095557f30f841f98fb317f9e
Ghaith Al Seirawan <m.g.alseirawan@gmail.com>
2023-02-02 11:38:57 +0100

    Added average minimum nights graph, fixing errors, finilizing room type per borough graph, and added PCP graph.

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
-
 from data import *
-
 
 mapbox_access_token = "pk.eyJ1IjoiZ2hhaXRoOTMiLCJhIjoiY2xkM2RkZXE5MGg5bTNvbDBpdnF3YmtpYyJ9.4AMiIng35KKFYqZ9jD3a9Q"
 
@@ -54,7 +52,7 @@ app.layout = html.Div([
 						step = 100,
 						value=df['price'].min(),
 						id='price-slider',
-						marks={i: f'${i}' for i in range(df['price'].min(), df['price'].max(), 150)}
+						marks={i: f'${i}' for i in range(df['price'].min(), df['price'].max()+50, 150)}
         		)
         		], style = {  'padding': '0px 10px 0px 8px'}),
 
@@ -78,7 +76,7 @@ app.layout = html.Div([
 
         html.Div([
 			dcc.Graph(id='room-type-bar'),
-            dcc.Graph(id='price-histogram')
+            dcc.Graph(id='night_average_figure')
             ], style={'width': '30%', 'float': 'right'}
 		)
 		], style={'display': 'inline-block', 'flex-direction': 'column'}
@@ -86,6 +84,10 @@ app.layout = html.Div([
 
 	html.Div([
 		dcc.Graph(id="borough_neighbourhood_counts")
+	],  style={'display': 'inline', 'flex-direction': 'column'}),
+
+	html.Div([
+		dcc.Graph(id="pcp_review_rate")
 	],  style={'display': 'inline', 'flex-direction': 'column'})
 ])
 
@@ -93,13 +95,14 @@ app.layout = html.Div([
 @app.callback(
     [
 		Output('map', 'figure'),
-     	Output('price-histogram', 'figure'),
+     	Output('night_average_figure', 'figure'),
      	Output('room-type-bar', 'figure'),
 		Output('total_listing', component_property='children'),
 		Output('out_of_total',  component_property='children'),
 		Output('average_price',  component_property='children'),
 		Output("cancelation_policy_pie_chart", "figure"),
-		Output("borough_neighbourhood_counts", 'figure')
+		Output("borough_neighbourhood_counts", 'figure'),
+		Output("pcp_review_rate", "figure")
 	],
     [
 		Input('neighborhood-dropdown', 'value'),
@@ -127,7 +130,6 @@ def update_graph(neighborhood, price_range, room_type):
 	# Create borough neighbourhood counts graph
 	borough_neighbourhood_counts = filtered_df.groupby(['borough', 'neighbourhood']).size().reset_index()
 	borough_neighbourhood_counts.columns = ['Borough', 'Neighbourhood', 'Count']
-
 	borough_neighbourhood_data = [go.Bar(
 		x = borough_neighbourhood_counts['Neighbourhood'],
 		y = borough_neighbourhood_counts['Count'],
@@ -143,16 +145,18 @@ def update_graph(neighborhood, price_range, room_type):
 			height = 500, 
 			title = "Listings Counts",
 			margin = dict(l =50, r = 50, t = 50, b = 50),
-			
 		)
 	borough_neighbourhood_graph = go.Figure(data = borough_neighbourhood_data, layout = borough_neighbourhood_layout)
+	
 	# Create a map of the listings
 	data = [go.Scattermapbox(
 	    lat=filtered_df["lat"],
 	    lon=filtered_df["long"],
 	    mode="markers",
 	    marker=go.scattermapbox.Marker(
-	        size = 3,
+	        size = 4,
+			sizemode = 'diameter',
+			sizeref = 0.1,
 	       	color = filtered_df["review_rate_number"],
 	        colorscale = 'Viridis',
 	        colorbar = dict(thickness = 10, title = "Review rate"),
@@ -179,33 +183,38 @@ def update_graph(neighborhood, price_range, room_type):
 			
 	    ),
 	)
-
 	figure = go.Figure(data=data, layout=layout)
 
-	# Create a histogram of the prices
-	price_histogram_trace = go.Histogram(x=filtered_df['price'], nbinsx=20)
-	price_histogram_layout = go.Layout(
-		title='Price Histogram', 
-		margin = dict(l =50, r = 50, t = 50, b = 50),
-		height = 350, 
-		)
-	price_histogram_figure = go.Figure(data=[price_histogram_trace], layout=price_histogram_layout)
-
 	# Create a bar chart of the room types
-	room_type_counts = df.groupby(["room_type", "instant_bookable"]).size().reset_index()
-	room_type_counts.columns = ["room_type", "instant_bookable", "count"]
-
+	room_type_counts = filtered_df.groupby(["room_type", "borough"]).size().reset_index(name='counts')
 	room_type_bar_trace = px.bar(
-			x=room_type_counts["room_type"], 
-			y=room_type_counts["count"],
-			color = room_type_counts["instant_bookable"],
+			room_type_counts,
+			y="counts",
+			x="room_type",
+			color = "borough",
+			height = 350,
+			title = "Room Type per Borough Count"
+
 		)
-	room_type_bar_layout = go.Layout(
-		title='Room Type Counts',	
+	room_type_bar_layout = go.Layout(	
 		margin = dict(l =50, r = 50, t = 50, b = 50),
-		height = 300,
 		)
 	room_type_bar_figure = go.Figure(data=room_type_bar_trace, layout=room_type_bar_layout)
+
+	# Create a histogram of the Average Minimum Nights per Room Type by Borough
+	df_average_MN_ = filtered_df.groupby(["room_type", "borough"]).agg({"minimum_nights":"mean"}).reset_index()
+	night_average_trace = px.bar(
+		df_average_MN_,
+		x = "room_type",
+		y = "minimum_nights",
+		color = "borough",
+		title='Average Minimum Nights', 
+		height = 350, 
+	)
+	night_average_layout = go.Layout(
+		margin = dict(l =50, r = 50, t = 50, b = 50)
+		)
+	night_average_figure = go.Figure(data=night_average_trace, layout=night_average_layout)
 
 	# cancelation_policy_pie_chart
 	colors = ['gold', 'mediumturquoise', 'lightgreen']
@@ -232,7 +241,15 @@ def update_graph(neighborhood, price_range, room_type):
         marker=dict(colors=colors, line=dict(color='#000000', width=2))
 		)
 
-	return figure, price_histogram_figure, room_type_bar_figure, total_listing, out_of_total, average_price, cancellation_pie, borough_neighbourhood_graph
+	pcp_review_rate_ = px.parallel_coordinates(
+		filtered_df,
+		color = "review_rate_number",
+		dimensions = ["number_of_reviews", "review_rate_number", "minimum_nights"],
+		color_continuous_scale=px.colors.sequential.Plasma,
+		title="Parallel Coordinate Plot of Number of Reviews, Review Rate Number, and Minimum Nights"
+	)
+	pcp_review_rate_plot = go.Figure(data = pcp_review_rate_)
+	return figure, night_average_figure, room_type_bar_figure, total_listing, out_of_total, average_price, cancellation_pie, borough_neighbourhood_graph, pcp_review_rate_plot
 
 if __name__ == "__main__":
     app.run_server(debug=True)
